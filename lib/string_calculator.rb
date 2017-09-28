aaac3d80ec1fdb37751791c14a6298c25da8ced5
anne <richardson.ae@gmail.com>
2017-09-28 12:45:13 -0500

    Write logic for multiply method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -8,6 +8,10 @@ class StringCalculator
     nums == '' ? 0 : nums.split(',').map(&:to_i).reduce(:-)
   end
 
+  def self.multiply(nums='')
+    nums == '' ? 0 : nums.split(',').map(&:to_i).reduce(:*)
+  end
+
 end
 
 # p StringCalculator.add('1,6')
