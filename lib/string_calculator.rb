acb0be2ffd8d64926d301d37db2c39f67ea61e09
anne <richardson.ae@gmail.com>
2017-09-28 12:37:35 -0500

    Add class logic for .subtract

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,10 @@ class StringCalculator
     nums == '' ? 0 : nums.split(',').map(&:to_i).reduce(:+)
   end
 
+  def self.subtract(nums='')
+    nums == '' ? 0 : nums.split(',').map(&:to_i).reduce(:-)
+  end
+
 end
 
 # p StringCalculator.add('1,6')
