124f701c349fc3e06836655f7f686c30eefe9bca
anne <richardson.ae@gmail.com>
2017-09-28 13:58:29 -0500

    Add logic for .divide

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -12,6 +12,43 @@ class StringCalculator
     nums == '' ? 0 : nums.split(',').map(&:to_i).reduce(:*)
   end
 
-end
+  def self.divide(nums='')
+
+    if is_empty(nums)
+      0
+    else
+      if has_1_input(nums)
+        as_integers(nums).reduce(:/)
+      else
+        num1 = nums.split(',')[0].to_i
+        num2 = nums.split(',')[1].to_i
+        if num1 % num2 == 0
+          as_integers(nums).reduce(:/)
+        else
+          as_floats(nums).reduce(:/)
+        end
+      end
+    end
+  end #divide
+
+  private
+
+  def self.is_empty(nums)
+    nums == ''
+  end
+
+  def self.has_1_input(nums)
+    nums.split(',').length == 1
+  end
+
+  def self.as_integers(nums)
+    nums.split(',').map(&:to_i)
+  end
+
+  def self.as_floats(nums)
+    nums.split(',').map(&:to_f)
+  end
+
+end #StringCalculator
 
 # p StringCalculator.add('1,6')
